31491af3f9856a49a8e15e8cf0c3609ee814da90
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-10-13 13:17:18 +1100

    creates compileAndRun script to compile and run from command line

---
+++ compileAndRun.sh
@@ -0,0 +1,4 @@
+cd ~/comp-sci/projects/kitkat-lang/java_project/app/src/main/java
+javac -d ../../../bin/main ./java_project/App.java
+cd ~/comp-sci/projects/kitkat-lang/java_project/app/bin/main
+java java_project.App
